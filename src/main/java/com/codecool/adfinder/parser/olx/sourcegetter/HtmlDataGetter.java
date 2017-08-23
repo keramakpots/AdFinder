6cb8f33c7e6f72172e1427474e77c7db1a2f6c3f
sebastiankurant <sebastian.kt7@gmail.com>
2017-08-23 19:49:05 +0200

    Create and implement HtmlDataGetter class to get data that is necessary for create Ad object

---
+++ src/main/java/com/codecool/adfinder/parser/olx/sourcegetter/HtmlDataGetter.java
@@ -0,0 +1,134 @@
+package com.codecool.adfinder.parser.olx.sourcegetter;
+
+import com.codecool.adfinder.parser.olx.utils.StreetFinderPattern;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+
+public class HtmlDataGetter {
+    private String url;
+    private String street;
+    private String description;
+    private String publishTime;
+    private Integer price;
+    private Integer amountOfRooms;
+    private Document source;
+    private String title;
+    private Map<String, String> properties;
+    private Integer streetLength;
+
+    public Integer getStreetLength() {
+        return streetLength;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPublishTime() {
+        return publishTime;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public Integer getAmountOfRooms() {
+        return amountOfRooms;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public HtmlDataGetter(String url) throws IOException {
+        setAdParameters(url);
+    }
+
+    private void setAdParameters(String url) throws IOException {
+        this.url = url;
+        source = getSourceFromUrl(this.url);
+        description = getDescriptionFromSource();
+        street = getStreetFromDescription();
+        properties = setProperties();
+        publishTime = getPublishTimeFromSource();
+        price = getPriceFromSource();
+        title = getTitleFromSource();
+        amountOfRooms = getRoomsFromSource();
+        streetLength = getLengthOfStreet();
+    }
+
+    private Integer getLengthOfStreet() {
+        if(street == null){
+            return null;
+        }
+        return street.length();
+    }
+
+    private Integer getRoomsFromSource() {
+        String rooms = properties.get("Liczba pokoi");
+        if (rooms.equals("Kawalerka")) {
+            return 1;
+        }
+        return Character.getNumericValue(rooms.charAt(0));
+    }
+
+    private String getStreetFromDescription() {
+        Matcher matcher = StreetFinderPattern.getInstance().getPattern().matcher(description);
+        if (matcher.find()) {
+            return matcher.group();
+        }
+        return null;
+    }
+
+    private String getTitleFromSource() {
+        return source.getElementsByTag("title").text();
+    }
+
+    private Integer getPriceFromSource() {
+        String priceWithCurrency = source.getElementsByClass("price-label").text().replaceAll("\\D+","");;
+        if(priceWithCurrency.length() == 0) {
+            return null;
+        }
+        return Integer.parseInt(priceWithCurrency);
+    }
+
+    // TODO: 16/08/2017 get proper time and date
+    // hour: \d{2}:\d{2}
+    // date: \d+\s\w+\s\d+
+    private String getPublishTimeFromSource() {
+        return source.getElementsByClass("offer-titlebox__details").select("em").text();
+    }
+
+    private Map<String, String> setProperties() {
+        List<String> category = source.getElementsByClass("item").select("th").eachText();
+        List<String> value = source.getElementsByClass("item").select("td").eachText();
+        Map<String, String> properties = new HashMap<>();
+        for (int i = 0; i < value.size(); i++) {
+            properties.put(category.get(i), value.get(i));
+        }
+        return properties;
+    }
+
+    private String getDescriptionFromSource() {
+        return source.getElementById("textContent").text();
+    }
+
+    private Document getSourceFromUrl(String url) throws IOException {
+        return Jsoup.connect(url).get();
+    }
+
+}
