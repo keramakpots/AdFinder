0e47f63bfbbea1f0553b9f62a8991c85d2039d75
sebastiankurant <sebastian.kt7@gmail.com>
2017-08-23 15:17:21 +0200

    Init spring project

---
+++ src/main/java/com/codecool/adfinder/AdfinderApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.adfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdfinderApplication.class, args);
+	}
+}
